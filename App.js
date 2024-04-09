e56503e8b213bbc9a97c0ab4dd2d7761a8db0395
luanaCristina <luanacrystinne@msn.com>
2024-04-08 21:10:31 -0300

    adicionando o nosso código refatorado no App.js

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button';
+import Row from './components/Row';
+import calculator, {initialState} from './util/calculator';
+import { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Luana</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  state = initialState;
+
+  HandleTap = (type, value) => {
+    this.setState((state) => calculator(type, value, state));
+  };
+
+  render(){
+    return (
+      <View style={styles.container}>
+       <SafeAreaView>
+        <Text style={styles.value}>
+          {parseFloat(this.state.currentValue).toLocaleString()}
+        </Text>
+        </SafeAreaView> 
+      </View>
+    )
+  }
+  
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
